ac37f130a67da6fca1562d1318d765cec67d017c
sheep <2353004506@qq.com>
2025-04-14 10:05:29 +0800

    更新

--- app/couponsfab/controller/ArticleController.php
+++ app/couponsfab/controller/ArticleController.php
@@ -46,4 +46,45 @@ class ArticleController extends HomeBaseController
         $this->assign(['tag' => $tag, 'free_stuff_list' => $freeStuffList, 'name' => $name]);
         return $this->fetch('free_stuff_list');
     }
+
+    public function freeStuffInfo()
+    {
+        $param = input();
+
+        $id = $param['id'];
+
+        // 获取所有标签
+        $tag = FreeStuffTagModel::getInstance()->select()->toArray();
+        $tag = array_column($tag, null, 'id');
+        // 获取文章内容
+        $freeStuffInfo = FreeStuffModel::getInstance()
+            ->where("id", $id)
+            ->find();
+
+        $freeStuffInfo['update_time'] = date("F d, Y", $freeStuffInfo['update_time']);
+
+        $freeStuffTag = $freeStuffInfo['tag'];
+        $freeStuffTag = explode(',', $freeStuffTag);
+        $freeStuffTag = $freeStuffTag[0] ?? 0;
+
+        $tagName = $tag[$freeStuffTag] ?? [];
+
+        // 获取当前标签下的文章列表
+        $freeStuffList = FreeStuffModel::getInstance()->whereRaw("FIND_IN_SET(?, tag)", [$freeStuffTag])->select()->toArray();
+        foreach ($freeStuffList as &$value) {
+            $value['update_time'] = date("F d, Y", $value['update_time']);
+
+            // 标签数据
+            $value['tag'] = explode(',', $value['tag']);
+            // 筛选匹配到的 tag
+            $value['tags'] = array_filter($tag, function ($item) use ($value) {
+                return in_array($item['id'], $value['tag']);
+            });
+        }
+
+
+        $this->assign(['tag' => $tag, 'free_stuff_info' => $freeStuffInfo, 'tag_name' => $tagName, 'free_stuff_list' => $freeStuffList]);
+
+        return $this->fetch('free_stuff_info');
+    }
 }
